1fb2dd21baee76f4d98f39257058e3980d0fb9cc
filipferris <filip.nicolaisen@outlook.com>
2020-05-02 13:05:59 +0200

    Updated hello

--- hello.py
+++ hello.py
@@ -1,12 +1,26 @@
 from flask import Flask
-# import testBot
-myapp = Flask(__name__)
+from datetime import datetime
+import re
 
-@myapp.route("/")
-def hello():
-    return "Hello Flask"
+app = Flask(__name__)
 
-@myapp.route("/")
-def run():
-    # await testBot.run()
-    return "Bot is running" 
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
